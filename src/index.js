08b5c2ebc4e168a14dd848d4fad9855723ae1659
demonjoub <demonjoub@gmail.com>
2018-05-31 00:17:06 +0700

    goble function
    goble function prototype

--- src/index.js
+++ src/index.js
@@ -6,3 +6,35 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
+String.prototype.replaceToX = function () {
+  return this.replace(/[a-zA-Z0-9-.]/g, 'x')
+}
+// prototype array max
+Array.prototype.max = function () {
+  return Math.max(...this)
+}
+// prototype array min
+Array.prototype.min = function () {
+  return Math.min(...this)
+}
+
+Array.prototype.diff = function (a) {
+  return this.filter(function (i) {
+    return a.indexOf(i) === -1;
+  });
+};
+
+
+Number.prototype.double = function () {
+  return this * 2
+}
+
+// Difference Between 2 JavaScript Arrays
+var a = [1,2,3,4,5,6,7,8,9]
+var b = [1,2,4,5,7,8,9]
+console.log(a.diff(b))
+
+console.log([1, 2, 3, 4, 5, 10].max())
+console.log([1, 2, 3, 4, 5, 10].min())
